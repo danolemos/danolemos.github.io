6739238d0511dd613bc4002aeb7f077a218ea374
danolemos <116593614+danolemos@users.noreply.github.com>
2026-04-16 19:27:32 -0300

    Create head.js
    --> Criando uma head injetável em todas as páginas
    
    OBS.: o arquivo em CSS chama-se style.min.css, aqui.

---
+++ head.js
@@ -0,0 +1,8 @@
+/*
+document.head.innerHTML += `
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="orquestra_acad_logo.png" type="image/png">
+  <link rel="stylesheet" href="style.min.css">
+`;
+*/
